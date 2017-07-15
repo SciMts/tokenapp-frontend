624c9bd21391a6394d18b86913dce26ca0241aba
Moritz Schneider <schneidermoritz90@gmail.com>
2017-07-15 13:52:23 +0200

    Created components

---
+++ src/components/MultiStepForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <signup></signup>
+  </div>
+</template>
+
+<script>
+
+import SignUp from './SignUp.vue'
+
+export default {
+  name: 'multistepform',
+  data () {
+    return {
+      msg: 'multistepform'
+    }
+  },
+  components: {
+    signup: SignUp
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
+import MultiStepForm from '@/components/MultiStepForm'
+import SignUp from '@/components/SignUp'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/form',
+      name: 'multistepform',
+      component: MultiStepForm
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
     }
   ]
 })
