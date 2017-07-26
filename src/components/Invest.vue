11904c0b12812cb676dddb36c9bdb8eeb26efe20
Moritz Schneider <schneidermoritz90@gmail.com>
2017-07-26 19:59:04 +0200

    Show error when no btc or ether address is displayed

--- src/components/Invest.vue
+++ src/components/Invest.vue
@@ -1,33 +1,43 @@
-<template xmlns:v-clipboard="http://www.w3.org/1999/xhtml">
+<template>
   <div>
-    <h3>Almost done!</h3>
-    <p>
-      You can send your Ether and Bitcoin from any source including Exchanges wallets. You can also send as often as you want.</p>
-    <p>
-      Don’t forget to bookmark the addresses to doublecheck the transaction status on  <a target="_blank" href="https://blockchain.info/">blockchain.info</a> or <a target="_blank" href="https://etherscan.io/">etherscan.io</a>
-    </p>
-
     <div class="row">
-      <div class="col-lg-6 col-sm-6 col-xs-6, col-md-6">
-        <h3>Bitcoin</h3>
-        <p v-model="btc">
-          <a target="_blank" :href="'https://blockchain.info/address/' + btc">{{ btc }}</a>
-        </p>
-
-        <img :src="'https://blockchain.info/qr?data=' + btc + '&size=200'"/>
-      </div>
-      <div class="col-lg-6 col-sm-6 col-xs-6, col-md-6">
-        <h3>Ether</h3>
-        <p>
-          <a target="_blank" :href="'https://etherscan.io/address/' + eth">{{ eth }}</a>
-        </p>
-        <img
-          :src="'https://chart.googleapis.com/chart?cht=qr&chl=' + eth + '&chs=200x200&choe=UTF-8&chld=L|2'"/>
+      <div class="col-xs-12">
+        <p class="bg-danger">{{errorMsg}}</p>
       </div>
     </div>
-    <div class="row">
-      <div class="col-lg-12">
-        <p class="bg-success"> Thank you very much for your contribution. You will receive an email, once the token is ready for distribution. In the meantime stay tuned to our progress on our <a target="_blank" href="https://medium.com/@modum_io">blog.</a> </p>
+    <div v-if="!this.errorMsg">
+      <h3>Almost done!</h3>
+      <p>
+        You can send your Ether and Bitcoin from any source including Exchanges wallets. You can also send as often as you want.</p>
+      <p>
+        Don’t forget to bookmark the addresses to doublecheck the transaction status on  <a
+        target="_blank" href="https://blockchain.info/">blockchain.info</a> or <a target="_blank"
+                                                                                  href="https://etherscan.io/">etherscan.io</a>
+      </p>
+
+      <div class="row">
+        <div class="col-lg-6 col-sm-6 col-xs-6, col-md-6">
+          <h3>Bitcoin</h3>
+          <p v-model="btc">
+            <a target="_blank" :href="'https://blockchain.info/address/' + btc">{{ btc }}</a>
+          </p>
+          <img :src="'https://blockchain.info/qr?data=' + btc + '&size=200'"/>
+        </div>
+        <div class="col-lg-6 col-sm-6 col-xs-6, col-md-6">
+          <h3>Ether</h3>
+          <p>
+            <a target="_blank" :href="'https://etherscan.io/address/' + ether">{{ ether }}</a>
+          </p>
+          <img
+            :src="'https://chart.googleapis.com/chart?cht=qr&chl=' + ether + '&chs=200x200&choe=UTF-8&chld=L|2'"/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <p class="bg-success">
+            Thank you very much for your contribution. You will receive an email, once the token is ready for distribution. In the meantime stay tuned to our progress on our <a
+            target="_blank" href="https://medium.com/@modum_io">blog.</a></p>
+        </div>
       </div>
     </div>
 
@@ -40,8 +50,16 @@
     data: function () {
       return {
         btc: '167ApWWxUSFQmz2jdz9xop3oAKdLejvMML',
-        eth: '0x32be343b94f860124dc4fee278fdcbd38c102d88'
+        ether: '0x32be343b94f860124dc4fee278fdcbd38c102d88',
+        errorMsg: ''
       }
+    },
+    mounted: function () {
+      if (!this.$root.store.btc && !this.$root.store.ether) {
+        this.errorMsg = 'Oops. Something is wrong.'
+      }
+      this.btc = this.$root.store.btc
+      this.ether = this.$root.store.ether
     }
   }
 </script>
