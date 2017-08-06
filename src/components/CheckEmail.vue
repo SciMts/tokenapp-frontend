46711d45c2ddee07a8ffb5ea18dfea8b429de415
Moritz Schneider <schneidermoritz90@gmail.com>
2017-08-06 12:47:18 +0200

    E-Mail to lowercase

--- src/components/CheckEmail.vue
+++ src/components/CheckEmail.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h2>Invite sent</h2>
-    <p>We've sent you an E-Mail with a confirmation link. Please check your E-Mail and proceed from there.</p>
+    <p>We've sent you an e-mail with a confirmation link. Please check your e-mail and proceed from there.</p>
   </div>
 </template>
