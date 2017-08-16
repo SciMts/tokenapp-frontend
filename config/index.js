a43e9aff8b9a845eba9905b1e91c75d1111fe3bc
Marc Heimgartner <marc.heimgartner@gmx.ch>
2017-08-16 14:58:31 +0200

    Changed proxy to local

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'https://token.modum.io',
+        target: 'http://127.0.0.1:8081',
         changeOrigin: true
       }
     },
