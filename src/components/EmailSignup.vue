6f2e4ccaade04608e02c2aa78d34a61c07b2d049
Jonny Burger <jonathanburger11@gmail.com>
2018-03-27 10:26:44 +0200

    Small tweaks

--- src/components/EmailSignup.vue
+++ src/components/EmailSignup.vue
@@ -8,7 +8,7 @@
           <textarea readonly style="width: 100%; height: 200px; resize: none; font-size: small;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
         <label for="terms-checkbox">
           <input type="checkbox" value="" v-model="terms" id="terms-checkbox">
-          I have read and accepted the terms of the MOD token sale
+          I have read and accepted the terms of the EKA token sale
         </label>
       </div>
       <div slot="footer">
