d6f22a04f6eb2c97d29155c6c7cb6c988c903685
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-07-06 18:16:30 +0200

    v-else on image and incoming type for closed icon

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -12,10 +12,10 @@
           <div v-bind:class="{ currentTier: tier.isCurrent }"  style="display: flex; flex-direction: column; align-items: center; padding: 12px">
             <div class="row blue">{{tier.name}}</div>
             <div class="row blue">
-              <div v-if="tier.amount === tier.maxAmount">
+              <div v-if="tier.amount === tier.maxAmount || tier.type === 'INCOMING'">
                 <svg style="height: 68px; margin-top: 10px" aria-hidden="true" data-prefix="fas" data-icon="box" class="svg-inline--fa fa-box fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#2174f3" d="M509.5 184.6L458.9 32.8C452.4 13.2 434.1 0 413.4 0H272v192h238.7c-.4-2.5-.4-5-1.2-7.4zM240 0H98.6c-20.7 0-39 13.2-45.5 32.8L2.5 184.6c-.8 2.4-.8 4.9-1.2 7.4H240V0zM0 224v240c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V224H0z"></path></svg>
               </div>
-              <div v-if="tier.amount < tier.maxAmount">
+              <div v-else>
                 <svg style="height: 68px; margin-top: 10px" aria-hidden="true" data-prefix="fas" data-icon="box-open" class="svg-inline--fa fa-box-open fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M53.2 41L1.7 143.8c-4.6 9.2.3 20.2 10.1 23l197.9 56.5c7.1 2 14.7-1 18.5-7.3L320 64 69.8 32.1c-6.9-.8-13.5 2.7-16.6 8.9zm585.1 102.8L586.8 41c-3.1-6.2-9.8-9.8-16.7-8.9L320 64l91.7 152.1c3.8 6.3 11.4 9.3 18.5 7.3l197.9-56.5c9.9-2.9 14.7-13.9 10.2-23.1zM425.7 256c-16.9 0-32.8-9-41.4-23.4L320 126l-64.2 106.6c-8.7 14.5-24.6 23.5-41.5 23.5-4.5 0-9-.6-13.3-1.9L64 215v178c0 14.7 10 27.5 24.2 31l216.2 54.1c10.2 2.5 20.9 2.5 31 0L551.8 424c14.2-3.6 24.2-16.4 24.2-31V215l-137 39.1c-4.3 1.3-8.8 1.9-13.3 1.9z"></path></svg>
               </div>
             </div>
@@ -202,7 +202,9 @@
 
       getRates: function () {
         axios.get(Vue.config.API + ratesEndPoint).then(response => {
-          // successfull
+          // set BTC/ETH prices
+          this.ethPrice = response.data.ethPrice
+          this.btcPrice = response.data.btcPrice
           return response
         }).catch(err => {
           return err
